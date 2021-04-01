4847937006519f2f534a24c665310c151fcf2f8d
osmanincir <osmanincir.bm@gmail.com>
2021-04-01 15:38:17 +0300

    added Menu Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Navbar, NavbarBrand} from "reactstrap";
+import Menu from './components/MenuComponent';
 
-export function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div>
+            <Navbar dark color="primary">
+                <div className="container">
+                    <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                </div>
+            </Navbar>
+            <Menu/>
+        </div>
+    );
+  }
 }
 
 export default App;
