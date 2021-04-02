bbb2da0aafb3fc36b65c2f6fbdf9bf995b0d11e7
osmanincir <osmanincir.bm@gmail.com>
2021-04-02 12:56:51 +0300

    dishes lifted up to App.js component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React, {Component} from 'react';
 import './App.css';
 import {Navbar, NavbarBrand} from "reactstrap";
 import Menu from './components/MenuComponent';
+import {DISHES} from "./shared/dishes";
 
 
 class App extends Component {
-  render() {
+    constructor(props) {
+        super(props);
+
+        this.state={
+            dishes:DISHES
+        }
+    }
+
+    render() {
     return (
         <div>
             <Navbar dark color="primary">
@@ -14,7 +23,8 @@ class App extends Component {
                 </div>
             </Navbar>
 
-            <Menu/>
+            <Menu dishes={this.state.dishes}/>
+
 
 
         </div>
