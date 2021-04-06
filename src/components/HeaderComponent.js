740462b77d43ad4921c3c1c537e5182be4a3c86c
osmanincir <osmanincir.bm@gmail.com>
2021-04-06 21:09:50 +0300

    react-redux-form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,15 @@
 import React,{Component} from "react";
-import {Navbar, NavbarBrand, Nav, NavbarToggler, Jumbotron, NavItem, Collapse} from "reactstrap";
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavbarToggler,
+    Jumbotron,
+    NavItem,
+    Collapse,
+    ModalHeader,
+    ModalBody, Modal, Button, FormGroup, Label, Input, Form
+} from "reactstrap";
 import {NavLink} from "react-router-dom";
 
 class Header extends Component{
@@ -7,14 +17,28 @@ class Header extends Component{
         super(props);
 
         this.toggleNav = this.toggleNav.bind(this)
+        this.toggleModal = this.toggleModal.bind(this)
+        this.handleLogin = this.handleLogin.bind(this)
         this.state={
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen: false
         };
     }
     toggleNav(){
         this.setState({
             isNavOpen: !this.state.isNavOpen
         });
+    }
+    toggleModal(){
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+    handleLogin=(event)=>{
+        this.toggleModal();
+        alert("username: "+ this.username.value + "Password: "+ this.password.value
+        + "Remember: "+ this.remember.checked);
+        event.preventDefault();
     }
     render() {
         return (
@@ -30,7 +54,7 @@ class Header extends Component{
                             <Nav navbar>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/home">
-                                        <span className="fa fa-home fa-lg"></span>Home
+                                        <span className="fa fa-home fa-lg"/>Home
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
@@ -45,8 +69,15 @@ class Header extends Component{
                                 </NavItem>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/contactus">
-                                        <span className="fa fa-address-card fa-lg"></span>Contact Us
+                                        <span className="fa fa-address-card fa-lg"/>Contact Us
                                     </NavLink>
+                                </NavItem>
+                            </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}>
+                                        <span className={"fa fa-sign-in fa-lg"}/> Login
+                                    </Button>
                                 </NavItem>
                             </Nav>
                         </Collapse>
@@ -62,7 +93,31 @@ class Header extends Component{
                         </div>
                     </div>
                 </Jumbotron>
-
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                       innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                       innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                           innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </div>
         );
     }
